008b8120d39b1da9c2fdb7af39b8fe92577ec9f1
ranjith-training <mirrorneuron.ai@gmail.com>
2021-01-17 18:43:53 +0530

    user input tracking

--- ideation/views.py
+++ ideation/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 def classify(request):
     if request.method == 'POST':
         description = request.POST['input']
+        print("user input")
+        print(description)
         result = pipe_clf.predict([str(description)])[0]
         if result == 1:
             messages.success(request, "requested task is automatable")
