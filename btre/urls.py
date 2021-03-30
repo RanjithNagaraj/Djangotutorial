83b59d4b8e8c71e2c8a52eb338430039485f1f1a
ranjith-training <mirrorneuron.ai@gmail.com>
2021-03-31 00:18:19 +0530

    status files

--- btre/urls.py
+++ btre/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('', include('pages.urls')),
-    #path('accounts/', include('accounts.urls')),
-    #path('listings/', include('listings.urls')),
-    #path('contacts/', include('contacts.urls')),
-    path('ideation/', include('ideation.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('listings/', include('listings.urls')),
+    path('contacts/', include('contacts.urls')),
+    #path('ideation/', include('ideation.urls')),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
